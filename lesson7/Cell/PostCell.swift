36c3998c931fcf9e456529f32e353085818ddcd0
Dmitrii <dmitrii.nazarov7703@gmail.com>
2024-08-14 23:06:28 +0700

    final

---
+++ lesson7/Cell/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  lesson7
+//
+//  Created by Dmitrii Nazarov on 11.08.2024.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- lesson7/ViewController.swift
+++ lesson7/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let users = UserData.mockData()
+    
+    lazy var tableView: UITableView = {
+        $0.dataSource = self
+        $0.delegate = self
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return $0
+    }(UITableView(frame: view.frame, style: .insetGrouped))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        title = "Friends"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
+}
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath )
+        let user = users[indexPath.row]
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = user.name
+        config.secondaryText = user.msgs
+        config.image = UIImage(systemName: user.image)
+        
+        cell.accessoryType = .disclosureIndicator
+        
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //let vc = SecondViewController()
+        //vc.userData = users[indexPath.row]
+        //navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+           return "new"
+       }
+}
 
+struct UserData {
+    let name: String
+    let msgs: String
+    let image: String
+    
+    static func mockData() -> [UserData] {
+        [
+        UserData(name: "Иван Обухов", msgs: "30 сообщений", image: "binoculars.fill"),
+        UserData(name: "Петр Ян", msgs: "20 сообщений", image: "bell.slash.fill"),
+        UserData(name: "Альберт Дьяковский", msgs: "15 сообщений", image: "bolt.slash.circle.fill"),
+        ]
+    }
 }
 
